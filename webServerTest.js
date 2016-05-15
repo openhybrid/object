497ba8fbe25103ed4528b94eb5053ae8c8c0b3f1
Valentin Heun <heun@mit.edu>
2016-05-15 09:17:58 -0400

    testServer to check connectivity on port 8080

---
+++ webServerTest.js
@@ -0,0 +1,52 @@
+/**
+ * @preserve
+ *
+ *                                     .,,,;;,'''..
+ *                                 .'','...     ..',,,.
+ *                               .,,,,,,',,',;;:;,.  .,l,
+ *                              .,',.     ...     ,;,   :l.
+ *                             ':;.    .'.:do;;.    .c   ol;'.
+ *      ';;'                   ;.;    ', .dkl';,    .c   :; .'.',::,,'''.
+ *     ',,;;;,.                ; .,'     .'''.    .'.   .d;''.''''.
+ *    .oxddl;::,,.             ',  .'''.   .... .'.   ,:;..
+ *     .'cOX0OOkdoc.            .,'.   .. .....     'lc.
+ *    .:;,,::co0XOko'              ....''..'.'''''''.
+ *    .dxk0KKdc:cdOXKl............. .. ..,c....
+ *     .',lxOOxl:'':xkl,',......'....    ,'.
+ *          .';:oo:...                        .
+ *               .cd,    ╔═╗┌─┐┬─┐┬  ┬┌─┐┬─┐   .
+ *                 .l;   ╚═╗├┤ ├┬┘└┐┌┘├┤ ├┬┘   '
+ *                   'l. ╚═╝└─┘┴└─ └┘ └─┘┴└─  '.
+ *                    .o.                   ...
+ *                     .''''','.;:''.........
+ *                          .'  .l
+ *                         .:.   l'
+ *                        .:.    .l.
+ *                       .x:      :k;,.
+ *                       cxlc;    cdc,,;;.
+ *                      'l :..   .c  ,
+ *                      o.
+ *                     .,
+ *
+ *             ╦ ╦┬ ┬┌┐ ┬─┐┬┌┬┐  ╔═╗┌┐  ┬┌─┐┌─┐┌┬┐┌─┐
+ *             ╠═╣└┬┘├┴┐├┬┘│ ││  ║ ║├┴┐ │├┤ │   │ └─┐
+ *             ╩ ╩ ┴ └─┘┴└─┴─┴┘  ╚═╝└─┘└┘└─┘└─┘ ┴ └─┘
+ *
+ * Created by valentin on 5/15/16.
+ *
+ * Copyright (c) 2015 Valentin Heun
+ *
+ * All ascii characters above must be included in any redistribution.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/libraries/objectDefaultFiles'));
+
+app.listen(8080);
+console.log("test server started on port 8080");

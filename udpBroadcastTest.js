55a0e1b7e5e2dfd939a3432085be0722fc7deba8
Valentin Heun <heun@mit.edu>
2016-05-16 10:22:25 -0400

    udpTest

---
+++ udpBroadcastTest.js
@@ -0,0 +1,102 @@
+/**
+ * @preserve
+ *
+ *                                     .,,,;;,'''..
+ *                                 .'','...     ..',,,.
+ *                               .,,,,,,',,',;;:;,.  .,l,
+ *                              .,',.     ...     ,;,   :l.
+ *                             ':;.    .'.:do;;.    .c   ol;'.
+ *      ';;'                   ;.;    ', .dkl';,    .c   :; .'.',::,,'''.
+ *     ',,;;;,.                ; .,'     .'''.    .'.   .d;''.''''.
+ *    .oxddl;::,,.             ',  .'''.   .... .'.   ,:;..
+ *     .'cOX0OOkdoc.            .,'.   .. .....     'lc.
+ *    .:;,,::co0XOko'              ....''..'.'''''''.
+ *    .dxk0KKdc:cdOXKl............. .. ..,c....
+ *     .',lxOOxl:'':xkl,',......'....    ,'.
+ *          .';:oo:...                        .
+ *               .cd,    ╔═╗┌─┐┬─┐┬  ┬┌─┐┬─┐   .
+ *                 .l;   ╚═╗├┤ ├┬┘└┐┌┘├┤ ├┬┘   '
+ *                   'l. ╚═╝└─┘┴└─ └┘ └─┘┴└─  '.
+ *                    .o.                   ...
+ *                     .''''','.;:''.........
+ *                          .'  .l
+ *                         .:.   l'
+ *                        .:.    .l.
+ *                       .x:      :k;,.
+ *                       cxlc;    cdc,,;;.
+ *                      'l :..   .c  ,
+ *                      o.
+ *                     .,
+ *
+ *             ╦ ╦┬ ┬┌┐ ┬─┐┬┌┬┐  ╔═╗┌┐  ┬┌─┐┌─┐┌┬┐┌─┐
+ *             ╠═╣└┬┘├┴┐├┬┘│ ││  ║ ║├┴┐ │├┤ │   │ └─┐
+ *             ╩ ╩ ┴ └─┘┴└─┴─┴┘  ╚═╝└─┘└┘└─┘└─┘ ┴ └─┘
+ *
+ * Created by valentin on 5/16/16.
+ *
+ * Copyright (c) 2015 Valentin Heun
+ *
+ * All ascii characters above must be included in any redistribution.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const beatPort = 52316;
+var counter = 0;
+var HOST = '255.255.255.255';
+const beatInterval = 1000;
+var uuid = uuidTimeShort();
+var dgram = require('dgram');
+
+var udpServer = dgram.createSocket("udp4");
+udpServer.on("error", function (err) {
+    console.log("server error:\n" + err);
+    udpServer.close();
+});
+
+udpServer.on("message", function (msg) {
+    console.log("Message received: " + msg);
+    //msgContent = JSON.parse(msg);
+});
+
+udpServer.on("listening", function () {
+    var address = udpServer.address();
+    console.log("I am waiting for messages on port: " + address.port);
+});
+
+udpServer.bind(beatPort);
+
+var client = dgram.createSocket('udp4');
+client.bind(function () {
+    client.setBroadcast(true);
+    client.setTTL(2);
+    client.setMulticastTTL(2);
+});
+
+setInterval(function () {
+
+    var testMSG = "test with ID: " + uuid;
+
+    var message = new Buffer(
+        testMSG
+    );
+
+    client.send(message, 0, message.length, beatPort, HOST, function (err) {
+        if (err) {
+            console.log("I run in to an error. Message could not be send");
+            throw err;
+        }
+        // client is not being closed, as the beat is send ongoing
+    });
+
+}, beatInterval);
+
+function uuidTimeShort() {
+    var dateUuidTime = new Date();
+    var abcUuidTime = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var stampUuidTime = parseInt("" + dateUuidTime.getMilliseconds() + dateUuidTime.getMinutes() + dateUuidTime.getHours() + dateUuidTime.getDay()).toString(36);
+    while (stampUuidTime.length < 8) stampUuidTime = abcUuidTime.charAt(Math.floor(Math.random() * abcUuidTime.length)) + stampUuidTime;
+    return stampUuidTime
+}
